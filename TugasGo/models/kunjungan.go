06f907f94a5e58fbabbafc8656427e350b7f9d0b
Tery <95428895+TeryRama@users.noreply.github.com>
2024-07-01 02:25:29 +0700

    Update kunjungan.go

--- TugasGo/models/kunjungan.go
+++ TugasGo/models/kunjungan.go
@@ -1,15 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Kunjungan struct {
-	ID               uint      `json:"id" gorm:"primary_key"`
+	gorm.Model                 // Ini termasuk ID, CreatedAt, UpdatedAt, DeletedAt secara default
 	IDPasien         uint      `json:"id_pasien"`
 	Pasien           Pasien    `json:"pasien" gorm:"foreignKey:IDPasien"`
 	IDDokter         uint      `json:"id_dokter"`
 	Dokter           Dokter    `json:"dokter" gorm:"foreignKey:IDDokter"`
 	TanggalKunjungan time.Time `json:"tanggal_kunjungan"`
 	Keluhan          string    `json:"keluhan"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
 }
